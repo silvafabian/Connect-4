cbce2943cb95d04f6c4aea2ac8b81200602ceaef
Fabian Silva <silvafabian74@yahoo.com>
2022-02-15 17:59:18 -0500

    fix board bug

--- js/app.js
+++ js/app.js
@@ -1,15 +1,27 @@
 /*-------------------------------- Constants --------------------------------*/
 
 const winningCombos = [
-  [0, 1, 2, 3], [3, 4, 5, 6], [7, 8, 9, 10], [10, 11, 12, 13], [14, 15, 16, 17], 
-  [17, 18, 19, 20], [21, 22, 23, 24], [24, 25, 26, 27], [28, 29, 30, 31], [31, 32, 33, 34], [35, 36, 37, 38], [38, 39, 40, 41], [0, 7, 14, 21], [1, 8, 15, 22], [2, 9, 16, 23], 
-  [3, 10, 17, 24], [4, 11, 18, 25], [5, 12, 19, 26], [6, 13, 20, 27], [14, 21, 28, 35], 
-  [15, 22, 29, 36], [16, 23, 30, 37], [17, 24, 31, 38], [18, 25, 32, 39], [19, 26, 33, 40], [20, 27, 34, 41], [14, 22, 30, 38], [15, 23, 31, 39], [16, 24, 32, 40], [17, 25, 33, 41], [17, 23, 29, 35], [18, 24, 30, 36], [19, 25, 31, 37], [20, 26, 32, 38], [10, 16, 22, 28], [11, 17, 23, 29], [12, 18, 24, 30], [13, 19, 25, 31], [7, 15, 23, 31], [8, 16, 24, 32], 
-  [9, 17, 25, 33], [10, 18, 26, 34], [0, 8, 16, 24], [1, 9, 17, 25], [2, 10, 18, 26], 
-  [3, 11, 19, 27], [3, 9, 15, 21], [4, 10, 16, 22], [5, 11, 17, 23], [6, 12, 18, 24], 
-  [36, 37, 38, 39], [37, 38, 39, 40], [29, 30, 31, 32], [30, 31, 32, 33], [22, 23, 24, 25], [23, 24, 25, 26], [15, 16, 17, 18], [16, 17, 18, 19], [8, 9, 10, 11], [9, 10, 11, 12], 
-  [1, 2, 3, 4], [2, 3, 4, 5], [7, 14, 21, 28], [8, 15, 22, 29], [9, 16, 23, 30], 
-  [10, 17, 24, 31], [11, 18, 25, 32], [12, 19, 26, 33], [13, 20, 27, 34]
+  [7, 8, 9, 10], [8, 9, 10, 11], [9, 10, 11, 12], [10, 11, 12, 13], 
+  [14, 15, 16, 17], [15, 16, 17, 18], [16, 17, 18, 19], [17, 18, 19, 20],
+  [21, 22, 23, 24], [22, 23, 24, 25], [23, 24, 25, 26], [24, 25, 26, 27], 
+  [28, 29, 30, 31], [29, 30, 31, 32], [30, 31, 32, 33], [31, 32, 33, 34],
+  [35, 36, 37, 38], [36, 37, 38, 39], [37, 38, 39, 40], [38, 39, 40, 41],
+  [42, 43, 44, 45], [43, 44, 45, 46], [44, 45, 46, 47], [45, 46, 47, 48],
+  [7, 14, 21, 28], [14, 21, 28, 35], [21, 28, 35, 42], 
+  [8, 15, 22, 29], [15, 22, 29, 36], [22, 29, 36, 43],
+  [9, 16, 23, 30], [16, 23, 30, 37], [23, 30, 37, 44],
+  [10, 17, 24, 31], [17, 24, 31, 38], [24, 31, 38, 45],
+  [11, 18, 25, 32], [18, 25, 32, 39], [25, 32, 39, 46],
+  [12, 19, 26, 33], [19, 26, 33, 40], [26, 33, 40, 47],
+  [13, 20, 27, 34], [20, 27, 34, 41], [27, 34, 41, 48],
+  [28, 22, 16, 10], [35, 29, 23, 17], [29, 23, 17, 11],
+  [42, 36, 30, 24], [36, 30, 24, 18], [30, 24, 18, 12],
+  [43, 37, 31, 25], [37, 31, 25, 19], [31, 25, 19, 13],
+  [44, 38, 32, 26], [38, 32, 26, 20], [45, 39, 33, 27],
+  [21, 29, 37, 45], [14, 22, 30, 38], [22, 38, 30, 46],
+  [7, 15, 23, 31], [15, 23, 31, 39], [23, 31, 39, 47],
+  [8, 16, 24, 32], [16, 24, 32, 40], [48, 40, 32, 24],
+  [9, 17, 25, 33], [17, 25, 33, 41], [10, 18, 26, 34],
 ]
 
 /*-------------------------------- Variables --------------------------------*/
@@ -19,7 +31,7 @@ let board, turn, winner, numOfTurns
 
 /*------------------------ Cached Element References ------------------------*/
 
-const boardArr =document.querySelectorAll(".circle")
+const boardArr = document.querySelectorAll(".circle")
 
 const message = document.querySelector(".message")
 
@@ -64,8 +76,7 @@ function init() {
   numOfTurns = 0
   playing = false
   message.textContent = 'Press the Play button to get started!'
-  playBtn.hidden = true
-  // play()
+
   render()
 }
 
@@ -81,7 +92,11 @@ function clickBoard(evt) {
   if (winner || board[idxCircle] === -1 || board[idxCircle] ===1) {
     return
   }
-  
+  //TODO SHOW OFF THIS CODE FOR PRESENTATION
+  if (actualIdx <= 6) {
+    return
+  }
+
   turn *= -1
 
   checkTurn()
@@ -121,6 +136,7 @@ function choiceToBottom(idx) {
     if (board[i] === null) {
       return i
     }
+    
   }
 }
 
@@ -145,11 +161,13 @@ function getWinner() {
       message.textContent = "Player 1 wins!!!"
       winner = 'R'
       confetti.start(3000)
+      return
     }
     else if (board[a] +board[b] + board[c] + board[d]=== -4){
       message.textContent = "Player 2 wins!!!"
       winner = 'Y'
       confetti.start(3000)
+      return
     }
   } 
 
@@ -161,24 +179,13 @@ function getWinner() {
 
 function reset() {
   resetBtn.hidden = true
-  // playBtn.hidden = false
+  playBtn.hidden = false
   init()
 }
 
-function play() {
+function play() {  
+  playBtn.hidden = true
   init()
-  message.textContent = "Player 1 goes first"
-  
-  // when the button is hit, i want two thing to happen
-    // the message needs to change so that player 1 is up first
-    
-    // the init function needs to be called 
-    
-  // once there is a game in progress
-    // i need to hide the play button so it is not clicked again 
-    // the reset function will bring back the play button
-  
-
 }
 
 function theme() {
